09a3588564c87093f5e82daaa405a9329de5bf4f
aamhimel <aam.himel@gmail.com>
2021-03-06 01:41:42 +0600

    login page initial done, tailwind initialization is next

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/FirebaseContext';
 
@@ -6,9 +6,18 @@ const Login = () => {
     
     const history = useHistory();
     const { firebase } = useContext(FirebaseContext);
-    
 
-    document.title = 'login - page';
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const [error, setError   ] = useState('');
+    const isValid = password === '' || email === '';
+
+    // uses for updating page title`
+    useEffect(() => {
+        document.title = 'Login - Instagram'
+    }, [])
+
     return (
         <div>
             <p>Login details page</p>
